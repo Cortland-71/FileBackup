08c4ba43b8d594be288a1b42f5b2a3264b6ae80e
XB71 <32171659+XB71@users.noreply.github.com>
2019-06-27 14:36:28 -0700

    Back up

---
+++ BackUp.ps1
@@ -0,0 +1,18 @@
+﻿#sets time and date var for file 
+$date = Get-Date -UFormat "%m-%d-%y@%H-%M-%S"
+
+#Backup
+robocopy \\Server1\folder\file \\Server2\folder\fileBackup-$date /E
+
+#This will clear out old backups at specified amounts
+
+$backupCheck = Get-ChildItem '\\Server2\folder'
+
+#this will clear out the oldest possible backup while keeping 3 weeks worth of the newest backups
+
+if ($backupCheck.Count -gt 84)
+{
+    Get-ChildItem '\\Server2\folder' | Sort-Object CreationTime | Select-Object -First 1 | Remove-Item -Force -Recurse
+    
+}
+
